eb7d317734ec3c3079f491d1394ba95205f108c1
pilarsen <pilarsen@studio-aw.ru>
2023-11-06 12:42:48 +0300

    fix handler

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -10,13 +10,14 @@ user_router = Router()
 config = load_config()
 
 
-@user_router.message(F.text.contains(F.text.lower('видел')))
+@user_router.message(F.text.contains('видел'))
+@user_router.message(F.text.contains('Видел'))
 async def user_start(message: Message):
     if message.from_user.id == config.misc.raf_id:
-        await message.reply(f'Всем похуй, иди на хуй!')
+        await message.reply(f'Всем похуй, иди на хуй, @{message.from_user.username}!')
 
 
 @user_router.message(F.text.lower() == 'видел')
 async def user_start(message: Message):
     if not message.from_user.id in config.tg_bot.admin_ids:
-        await message.reply(f'Всем похуй, иди на хуй!')
+        await message.reply(f'Всем похуй, иди на хуй, @{message.from_user.username}!')
